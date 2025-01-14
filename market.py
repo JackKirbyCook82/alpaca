589129b1467cf3d378f15d3f8ded9339c6453b63
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-01-13 21:25:12 -0600

    Updated 1/10/2024

---
+++ market.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 2025
+@name:   Aplaca Market Objects
+@author: Jack Kirby Cook
+
+"""
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = []
+__copyright__ = "Copyright 2025, Jack Kirby Cook"
+__license__ = "MIT License"
+
+
+
+
+
+
+
