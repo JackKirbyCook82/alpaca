3d5b225d3339b743e641829c12a489a9d890e146
JackKirbyCook82 <jackcookchee@yahoo.com>
2025-03-01 05:04:13 -0600

    Updated 1/10/2024

--- orders.py
+++ orders.py
@@ -8,10 +8,12 @@ Created on Tues Feb 18 2025
 
 import numpy as np
 import pandas as pd
+from collections import namedtuple as ntuple
 
 from finance.variables import Querys, Variables, Securities, OSI
 from webscraping.weburl import WebURL, WebPayload
 from webscraping.webpages import WebJSONPage
+from webscraping.webdatas import WebJSON
 from support.decorators import ValueDispatcher
 from support.mixins import Emptying, Logging
 
@@ -29,41 +31,74 @@ class AlpacaOrderURL(WebURL, domain="https://paper-api.alpaca.markets", path=["v
         return {"APCA-API-KEY-ID": str(api.identity), "APCA-API-SECRET-KEY": str(api.code)}
 
 
-class AlpacaOrderPayload(WebPayload, arguments=["price", "tenure", "term"], parameters={"qty": 1, "order_class": "mleg"}, multiple=False, optional=False):
+class AlpacaOrderPayload(WebPayload, parameters={"qty": "1", "order_class": "mleg", "extended_hours": "false"}, multiple=False, optional=False):
     terms = {Variables.Markets.Terms.MARKET: "market", Variables.Markets.Terms.LIMIT: "limit"}
     tenures = {Variables.Markets.Tenure.DAY: "day", Variables.Markets.Tenure.FILLKILL: "fok"}
 
     @ValueDispatcher(locator="term")
-    def price(cls, *args, term, **kwargs): raise ValueError(term)
+    def price(self, *args, term, **kwargs): raise ValueError(term)
     @price.register(Variables.Markets.Terms.LIMIT)
-    def limit(cls, *args, price, **kwargs): return {"limit_price": f"{price:.02}"}
+    def limit(self, *args, price, **kwargs): return {"limit_price": f"{price:.02f}"}
     @price.register(Variables.Markets.Terms.MARKET)
-    def market(cls, *args, **kwargs): return {}
+    def market(self, *args, **kwargs): return {}
 
-    def tenure(cls, *args, tenure, **kwargs): return {"time_in_force": cls.tenures[tenure]}
-    def term(cls, *args, term, **kwargs): return {"type": cls.terms[term]}
+    def tenure(self, *args, tenure, **kwargs): return {"time_in_force": self.tenures[tenure]}
+    def term(self, *args, term, **kwargs): return {"type": self.terms[term]}
+    def execute(self, *args, **kwargs):
+        tenure = self.tenure(*args, **kwargs)
+        term = self.term(*args, **kwargs)
+        price = self.price(*args, **kwargs)
+        return dict(tenure) | dict(term) | dict(price)
 
-    class AlpacaLegPayload(WebPayload, key="securities", locator="legs", arguments=["security", "quantity", "action"], multiple=True, optional=False):
-        quantities = {Variables.Securities.Instrument.STOCK: 100, Variables.Securities.Instrument.OPTION: 1}
+    class AlpacaSecurityPayload(WebPayload, key="securities", locator="legs", multiple=True, optional=False):
+        quantities = {Variables.Securities.Instrument.STOCK: "100", Variables.Securities.Instrument.OPTION: "1"}
         actions = {Variables.Securities.Position.LONG: "buy", Variables.Securities.Position.SHORT: "sell"}
 
         @ValueDispatcher(locator="security")
-        def security(cls, *args, security, **kwargs): raise ValueError(security)
-        @security.register(Securities.Options)
-        def option(cls, *args, security, ticker, expire, strike, **kwargs): return {"symbol": OSI([ticker, expire, security.option, strike])}
-        @security.register(Securities.Stocks)
-        def stock(cls, *args, ticker, **kwargs): return {"symbol": str(ticker).upper()}
+        def security(self, *args, security, **kwargs): raise ValueError(security)
+        @security.register(*list(Securities.Options))
+        def option(self, *args, security, ticker, expire, strike, **kwargs): return {"symbol": str(OSI([ticker, expire, security.option, strike]))}
+        @security.register(*list(Securities.Stocks))
+        def stock(self, *args, ticker, **kwargs): return {"symbol": str(ticker).upper()}
 
-        def quantity(cls, *args, security, **kwargs): return {"ratio_qty": cls.quantities[security.instrument]}
-        def action(cls, *args, security, **kwargs): return {"side": cls.actions[security.poistion]}
+        def quantity(self, *args, security, **kwargs): return {"ratio_qty": self.quantities[security.instrument]}
+        def action(self, *args, security, **kwargs): return {"side": self.actions[security.position]}
+        def execute(self, *args, **kwargs):
+            security = self.security(*args, **kwargs)
+            quantity = self.quantity(*args, **kwargs)
+            action = self.action(*args, **kwargs)
+            return dict(security) | dict(quantity) | dict(action)
+
+
+class AlpacaOrderData(WebJSON, multiple=False, optional=False):
+    class Revenue(WebJSON.Text, locator="", key="revenue", parser=): pass
+    class Expense(WebJSON.Text, locator="", key="expense", parser=): pass
+    class Tenure(WebJSON.Text, locator="", key="tenure", parser=): pass
+    class Term(WebJSON.Text, locator="", key="term", parser=): pass
+    class Security(WebJSON, locator="", multiple=True, optional=True):
+        class Ticker(WebJSON.Text, locator="", key="ticker", parser=): pass
+        class Expire(WebJSON.Text, locator="", key="expire", parser=): pass
+        class Instrument(WebJSON.Text, locator="", key="instrument", parser=): pass
+        class Option(WebJSON.Text, locator="", key="option", parser=): pass
+        class Position(WebJSON.Text, locator="", key="position", parser=): pass
+        class Action(WebJSON.Text, locator="", key="action", parser=): pass
+        class Quantity(WebJSON.Text, locator="", key="quantity", parser=): pass
 
 
 class AlpacaOrderPage(WebJSONPage):
     def execute(self, *args, order, **kwargs):
         assert isinstance(order, dict)
-        url = self.AlpacaOrderURL(*args, **kwargs)
-        payload = self.AlpacaOrderPayload(*args, order=order, **kwargs)
-        self.load(url, *args, payload=payload, **kwargs)
+        url = AlpacaOrderURL(*args, **kwargs)
+        payload = AlpacaOrderPayload(order, *args, **kwargs)
+        self.load(url, *args, payload=payload.json, **kwargs)
+        data = AlpacaOrderData(self.json, *args, **kwargs)
+        contents = data(*args, **kwargs)
+        return contents
+
+
+class AlpacaStock(ntuple("Stock", "ticker security")): pass
+class AlpacaOption(ntuple("Option", "ticker expire security strike")): pass
+class AlpacaOrder(ntuple("Order", "term tenure cash stocks options")): pass
 
 
 class AlpacaOrderUploader(Emptying, Logging, title="Uploaded"):
@@ -75,7 +110,7 @@ class AlpacaOrderUploader(Emptying, Logging, title="Uploaded"):
         assert isinstance(prospects, pd.DataFrame)
         if self.empty(prospects): return
         for order in self.orders(prospects, *args, **kwargs):
-            self.page(*args, order=order, **kwargs)
+            order = self.page(*args, order=order, **kwargs)
 
     @staticmethod
     def orders(prospects, *args, term, tenure, **kwargs):
@@ -84,9 +119,10 @@ class AlpacaOrderUploader(Emptying, Logging, title="Uploaded"):
         for index, order in orders.iterrows():
             order = order.dropna(inplace=False)
             price = - np.round(float(order.spot), 2).astype(np.float32)
-            stocks = [dict(ticker=order.ticker, security=stock) for stock in order.strategy.stocks]
-            options = [dict(ticker=order.ticker, expire=order.expire, security=option, strike=order[option]) for option in order.strategy.options]
-            yield dict(term=term, tenure=tenure, price=price, securities=stocks + options)
+            stocks = [AlpacaStock(order.ticker, stock) for stock in order.strategy.stocks]
+            options = [AlpacaOption(order.ticker, order.expire, option, order[str(option)]) for option in order.strategy.options]
+            order = AlpacaOrder(term, tenure, price, stocks, options)
+            yield order
 
     @property
     def page(self): return self.__page
