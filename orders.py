753b35de847ab574d95ad26334f8c0d83a634757
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-05-16 05:28:28 -0500

    Updated 1/10/2024

---
+++ orders.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 2026
+@name:   Alpaca Order Objects
+@author: Jack Kirby Cook
+
+"""
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = []
+__copyright__ = "Copyright 2026, Jack Kirby Cook"
+__license__ = "MIT License"
+
+
+
