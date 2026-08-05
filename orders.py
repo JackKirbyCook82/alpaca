b95e374e42a797aab9cebc28b8dc6f9277172f74
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-08-05 14:41:27 -0500

    Updated 1/10/2024

--- orders.py
+++ orders.py
@@ -9,7 +9,7 @@ Created on Sat May 16 2026
 import multiprocessing
 import pandas as pd
 from parse import parse
-from abc import ABC, abstractmethod
+from datetime import date as Date
 
 from finance.enumerations import Instrument, Position, Status, Tenure, Terms, Intent
 from finance.logging import Logging
@@ -22,7 +22,7 @@ from webscraping.weburl import WebURL
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ["AlpacaOrderUploader", "AlpacaOrderDownloader", "AlpacaOrder"]
+__all__ = ["AlpacaOrderUploader", "AlpacaHeaders"]
 __copyright__ = "Copyright 2026, Jack Kirby Cook"
 __license__ = "MIT License"
 
@@ -37,7 +37,7 @@ position_formatter = lambda position: position_mapping[position, False]
 tenure_formatter = lambda tenure: tenure_mapping[tenure, False]
 term_formatter = lambda term: intent_mapping[term, False]
 quantity_formatter = lambda quantity: f"{quantity:.0f}"
-cost_formatter = lambda cost: f"{cost:.2f}"
+price_formatter = lambda price: f"{price:.2f}"
 
 timestamp_parser = lambda string: pd.to_datetime(string)
 ticker_parser = lambda string: OSI.parse(string).ticker
@@ -51,7 +51,11 @@ term_parser = lambda string: term_mapping[string, True]
 quantity_parser = lambda string: abs(int(string))
 
 
-AlpacaOrder = ["order", "created", "submitted", "filled", "expired", "canceled", "failed", "status", "tenure", "term", "asset", "ticker", "expire", "option", "strike", "position", "quantity"]
+class AlpacaHeaders:
+    Uploading = {"order": str, "asset": str, "spread": int, "ticker": str, "expire": Date, "option": int, "strike": float, "position": int, "quantity": int}
+    Downloading = {}
+
+
 class AlpacaOrderURL(WebURL, domain="https://paper-api.alpaca.markets", path=["v2", "orders"]):
     @staticmethod
     def headers(*args, authenticator, **kwargs):
@@ -65,7 +69,7 @@ class AlpacaDownloadingOrder(WebURL, parameters={"status": "all", "nested": True
 
 
 class AlpacaOrderPayload(WebPayload.Mapping, mapping={"order_class": "mleg", "qty": "1"}, multiple=False, optional=False):
-    class Cost(WebPayload.Value, key="cost", locator="limit_price", parser=cost_formatter): pass
+    class Price(WebPayload.Value, key="price", locator="limit_price", parser=price_formatter): pass
     class Tenure(WebPayload.Value, key="tenure", locator="time_in_force", parser=tenure_formatter): pass
     class Terms(WebPayload.Value, key="term", locator="type", parser=term_formatter): pass
     class Securities(WebPayload.Mapping, key="securities", locator="legs", multiple=True, optional=False):
@@ -96,39 +100,32 @@ class AlpacaOrderData(WebJSON.Mapping, multiple=False, optional=False):
         class Quantity(WebJSON.Text, key="quantity", locator="qty", parser=quantity_parser): pass
 
 
-class AlpacaOrderPage(WebJSONPage, ABC):
-    def __call__(self, *args, **kwargs):
-        data = self.execute(*args, **kwargs)
+class AlpacaUploadingOrderPage(WebJSONPage):
+    def execute(self, *args, acquisition, tenure, term, **kwargs):
+        parameters = dict(authenticator=self.authenticator)
+        url = AlpacaUploadingOrder(**parameters)
+        securities = [{"osi": record.osi, "position": record.position, "intent": (record.postion, acquisition.intent), "quantity": record.quantity} for record in acquisition.records]
+        payload = AlpacaOrderData({"price": acquisition.price, "tenure": tenure, "term": term, "securities": securities})
+        json = self.load(url, payload=payload)
+        data = AlpacaOrderData(json, *args, **kwargs)
         mapping = data(*args, **kwargs)
         records = mapping.pop("securities")
         records = [mapping | record for record in records]
         order = pd.DataFrame.from_records(records)
         order["expire"] = pd.to_datetime(order["expire"])
         order["strike"] = pd.to_numeric(order["strike"])
-        return order
-
-    @abstractmethod
-    def execute(self, *args, **kwargs): pass
-
-
-class AlpacaUploadingOrderPage(AlpacaOrderPage):
-    def execute(self, *args, spread, tenure, term, intent, **kwargs):
-        parameters = dict(authenticator=self.authenticator)
-        url = AlpacaUploadingOrder(**parameters)
-        securities = [{"osi": record.osi, "position": record.position, "intent": (record.postion, intent), "quantity": record.quantity} for record in spread.records]
-        payload = AlpacaOrderData({"cost": spread.cost, "tenure": tenure, "term": term, "securities": securities})
-        json = self.load(url, payload=payload)
-        data = AlpacaOrderData(json, *args, **kwargs)
-        return data
+        order["spread"] = acquisition.spread
+        columns = list(AlpacaHeaders.Uploading.keys())
+        return order[columns]
 
 
-class AlpacaDownloadingOrderPage(AlpacaOrderPage):
-    def execute(self, *args, order, **kwargs):
-        parameters = dict(authenticator=self.authenticator)
-        url = AlpacaDownloadingOrder(order=order, **parameters)
-        json = self.load(url)
-        data = AlpacaOrderData(json, *args, **kwargs)
-        return data
+# class AlpacaDownloadingOrderPage(AlpacaOrderPage):
+#     def execute(self, *args, order, **kwargs):
+#         parameters = dict(authenticator=self.authenticator)
+#         url = AlpacaDownloadingOrder(order=order, **parameters)
+#         json = self.load(url)
+#         data = AlpacaOrderData(json, *args, **kwargs)
+#         return data
 
 
 class AlpacaOrderUploader(WebStream, Logging, page=AlpacaUploadingOrderPage):
@@ -137,31 +134,32 @@ class AlpacaOrderUploader(WebStream, Logging, page=AlpacaUploadingOrderPage):
         self.__mutex = multiprocessing.Lock()
         self.__history = set()
 
-    def __call__(self, spreads, /, **kwargs):
-        assert isinstance(spreads, list)
-        if not bool(spreads): return pd.DataFrame(columns=AlpacaOrder)
-        generator = self.generator(spreads, **kwargs)
-        spreads = list(generator)
-        if not bool(spreads): return pd.DataFrame(columns=AlpacaOrder)
-        orders = self.uploader(spreads, **kwargs)
+    def __call__(self, acquisitions, /, **kwargs):
+        assert isinstance(acquisitions, list)
+        columns = list(AlpacaHeaders.Uploading.keys())
+        if not bool(acquisitions): return pd.DataFrame(columns=columns)
+        acquisitions = self.filter(acquisitions, **kwargs)
+        acquisitions = list(acquisitions)
+        if not bool(acquisitions): return pd.DataFrame(columns=columns)
+        orders = self.uploader(acquisitions, **kwargs)
         orders = pd.concat(list(orders), axis=0)
         orders = orders.sort_values(by=["order", "asset"], inplace=False)
         orders = orders.reset_index(drop=True, inplace=False)
         self.results(orders, title="Uploaded", instrument=Instrument.OPTION)
         return orders
 
-    def generator(self, spreads, /, **kwargs):
-        for spread in spreads:
-            if spread.signature in self.history: continue
-            with self.mutex: self.history.add(spread.signature)
-            yield spread
+    def filter(self, acquisitions, /, **kwargs):
+        for acquisition in acquisitions:
+            if acquisition.signature in self.history: continue
+            with self.mutex: self.history.add(acquisition.signature)
+            yield acquisition
 
-    def uploader(self, spreads, /, **kwargs):
-        for spread in spreads:
-            order = self.page(spread=spread, **kwargs)
-            securities = [f"{str(record.osi)}={int(record.position) * int(record.quantity):.0f}" for record in spread.records]
-            self.console("Uploaded", f"Propsect[{', '.join(securities)}]")
-            self.console("Uploaded", f"Prospect[Tight={spread.tightness:.2f}, Money={spread.moneyness:.2f}, Active={spread.activity:.2f}]")
+    def uploader(self, acquisitions, /, **kwargs):
+        for acquisition in acquisitions:
+            order = self.page(acquisition=acquisition, **kwargs)
+            securities = [f"{str(record.osi)}={int(record.position) * int(record.quantity):.0f}" for record in acquisition.records]
+            self.console("Uploaded", f"Acquisition[{', '.join(securities)}]")
+            self.console("Uploaded", f"Acquisition[Tight={acquisition.tightness:.2f}, Money={acquisition.moneyness:.2f}, Active={acquisition.activity:.2f}]")
             yield order
 
     @property
@@ -170,23 +168,23 @@ class AlpacaOrderUploader(WebStream, Logging, page=AlpacaUploadingOrderPage):
     def mutex(self): return self.__mutex
 
 
-class AlpacaOrderDownloader(WebStream, Logging, page=AlpacaDownloadingOrderPage):
-    def __call__(self, orders, **kwargs):
-        assert isinstance(orders, (list, str))
-        assert all([isinstance(order, str) for order in orders]) if isinstance(orders, list) else True
-        if isinstance(orders, str): orders = [orders]
-        if not bool(orders): return pd.DataFrame(columns=AlpacaOrder)
-        holdings = self.downloader(orders, **kwargs)
-        holdings = pd.concat(list(holdings), axis=0)
-        holdings = holdings.sort_values(by=["order", "asset"], inplace=False)
-        holdings = holdings.reset_index(drop=True, inplace=False)
-        self.results(holdings, title="Downloaded", instrument=Instrument.OPTION)
-        return holdings
-
-    def downloader(self, orders, /, **kwargs):
-        for order in set(orders):
-            holdings = self.page(order=order, **kwargs)
-            yield holdings
+# class AlpacaOrderDownloader(WebStream, Logging, page=AlpacaDownloadingOrderPage):
+#     def __call__(self, orders, **kwargs):
+#         assert isinstance(orders, (list, str))
+#         assert all([isinstance(order, str) for order in orders]) if isinstance(orders, list) else True
+#         if isinstance(orders, str): orders = [orders]
+#         if not bool(orders): return pd.DataFrame(columns=AlpacaOrder)
+#         holdings = self.downloader(orders, **kwargs)
+#         holdings = pd.concat(list(holdings), axis=0)
+#         holdings = holdings.sort_values(by=["order", "asset"], inplace=False)
+#         holdings = holdings.reset_index(drop=True, inplace=False)
+#         self.results(holdings, title="Downloaded", instrument=Instrument.OPTION)
+#         return holdings
+#
+#     def downloader(self, orders, /, **kwargs):
+#         for order in set(orders):
+#             holdings = self.page(order=order, **kwargs)
+#             yield holdings
 
 
 
